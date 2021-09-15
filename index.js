ee84665600f4eecabca91330923b8d5ee87aceb1
Andrew Osborn <osbornandrewj@gmail.com>
2021-09-15 15:00:18 -0700

    commit tree 330b4ae6c813be416adb9b876ff9c948fd770f36

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    y = customerName.toUpperCase();
+    customerName = y;
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'M.H.';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Dave';
+}
